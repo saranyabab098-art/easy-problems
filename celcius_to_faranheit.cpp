348a05a3d9bcfd119786b1b49822f6352f7f5fa2
saranyabab098-art <saranyabab098@gmail.com>
2025-08-29 23:42:19 +0530

    Add files via upload

---
+++ celcius_to_faranheit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c,f;
+    cout<<"enter degree in celcius ";
+    cin>>c;
+    f=(c*9/5)+32;
+    cout<<"the faranheit degree is "<<f;
+    return 0;
+
+
+}
+
