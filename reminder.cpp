f303c20a29a4345c1692fc64cb5550374cc97d54
saranyabab098-art <saranyabab098@gmail.com>
2025-08-29 23:44:39 +0530

    Add files via upload

---
+++ reminder.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,r;
+    cout<<"enter the dividant: ";
+    cin>>a;
+    cout<<"enter the divisor: ";
+    cin>>b;
+    r= a%b;
+    cout<<"reminder is "<<r;
+    return 0;
+
+
+
+}
+
