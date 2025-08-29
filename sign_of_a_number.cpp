283384cd7fcb6d31ada26dd7d63e43a04fcbd9d5
saranyabab098-art <saranyabab098@gmail.com>
2025-08-29 23:44:11 +0530

    Add files via upload

---
+++ sign_of_a_number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a number: ";
+    cin>>num;
+    if(num>0)
+    {
+    cout<<num<<"the number is positive";
+    }
+    else if(num<0)
+    {
+    cout<<num<<"the number is negative ";
+    }
+    else
+    {
+    cout<<num<<"the number is zero";
+    }
+    return 0;
+
+
+
+}
+
