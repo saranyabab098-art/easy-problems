bc3ceb18e1e17e3679027f608e59bc2b80e8970a
saranyabab098-art <saranyabab098@gmail.com>
2025-08-29 23:46:51 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number: ";
+    cin>>n ;
+    if(n%2==0)
+    {
+        cout<<"the number is even";
+
+    }
+    else
+    {
+        cout<<"the number is odd";
+    }
+    return 0;
+
+
+
+}
+
