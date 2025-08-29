f005cfb2c95ca2a6ce39de866576cd2c0d7968b0
saranyabab098-art <saranyabab098@gmail.com>
2025-08-29 23:46:33 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter number1: ";
+    cin>>a;
+    cout<<"enter number2: ";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"the value of the first number is "<<a<<endl;
+    cout<<"the value of the second number is "<<b<<endl;
+    return 0;
+
+
+
+}
+
