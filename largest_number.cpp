095e062353403376da7fef86e090fa92a4ab7267
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:20:22 +0530

    Add files via upload

---
+++ largest_number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"enter first number: ";
+ cin>>a;
+ cout<<"enter second number: ";
+ cin>>b;
+ if(a>b)
+ {
+  cout<<"the largest number is"<<a;
+
+ }
+ else
+ {
+  cout<<" the largest number is"<<b;
+ }
+ return 0;
+}
