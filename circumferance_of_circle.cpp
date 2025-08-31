b9d079733dcc0a09c5b819eda8663773bc7024df
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:25:22 +0530

    Add files via upload

---
+++ circumferance_of_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ float r,circ;
+ cout<<"enter the radius of circle: ";
+ cin>>r;
+ circ=2*3.14*r;
+ cout<<"the circumferance of a circle is "<<circ;
+ return 0;
+}
+
