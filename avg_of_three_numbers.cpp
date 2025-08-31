e4841b1c611f5cf97ed00567821168f67cf74516
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:20:56 +0530

    Add files via upload

---
+++ avg_of_three_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ float a,b,c,avg;
+ cout<<"enter first number: ";
+ cin>>a;
+ cout<<"enter second number: ";
+ cin>>b;
+ cout<<"enter third number: ";
+ cin>>c;
+ avg = (a+b+c)/3;
+ cout<<"the averge of three number is"<<avg;
+ return 0;
+}
