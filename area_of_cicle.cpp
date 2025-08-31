d9fb8fcad05581850e218c080dca9e939dadebec
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:24:58 +0530

    Add files via upload

---
+++ area_of_cicle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ float r,ar;
+ cout<<"the radius of a circle: ";
+ cin>>r;
+ ar=3.14*r*r;
+ cout<<"the area of a circle is "<<ar;
+ return 0;
+}
+
