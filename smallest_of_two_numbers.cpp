e3b4890deb7b7c781a4f2e1661978b77f239cd55
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:21:57 +0530

    Add files via upload

---
+++ smallest_of_two_numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"enter the first number: ";
+ cin>>a;
+ cout<<"enter the second number: ";
+ cin>>b;
+ if(a<b)
+ {
+  cout<<"the smallest number is"<<a;
+ }
+ else
+ {
+  cout<<"the smallest number is"<<b;
+ }
+ return 0;
+}
+
