46e3644b316219605fcbb7b79492d194a419ae3d
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:26:36 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ char ch;
+ int ans;
+ cout<<"enter a character: ";
+ cin>>ch;
+ ans=int(ch);
+ cout<<"the answer is "<<ans;
+ return 0;
+}
+
