0210765a95b92560b2abc94492f5ea4936ae8e7c
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:25:46 +0530

    Add files via upload

---
+++ power_of_a_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+ int a,b,ans;
+ cout<<"enter a number: ";
+ cin>>a;
+ cout<<"enter the exponent: ";
+ cin>>b;
+ ans=pow(a,b);
+ cout<<"the power of a number is "<<ans;
+ return 0;
+}
+
