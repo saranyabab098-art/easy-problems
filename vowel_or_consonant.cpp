b41c834a352ffae5a05fe94dcce49140d5dd31a4
saranyabab098-art <saranyabab098@gmail.com>
2025-08-31 13:32:32 +0530

    Add files via upload

---
+++ vowel_or_consonant.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ char ch;
+ cout<<"enter a character: ";
+ cin>>ch;
+ if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+ {
+  cout<<"the character is a vowel ";
+ }
+ else
+ {
+  cout<<"the character is not a vowel ";
+ }
+ return 0;
+
+}
+
+
+
+
